1feb068ac10625ef656999d560e0d80907098265
morrxy <morrxy@gmail.com>
2019-01-31 17:00:58 +0800

    add text field focus demo

--- lib/src/demo_list.dart
+++ lib/src/demo_list.dart
@@ -4,6 +4,7 @@ import 'drawer_demo.dart' as demo2 show title, makePage;
 import 'orientation_demo.dart' as demo4 show title, makePage;
 import 'tabs_demo.dart' as demo5 show title, makePage;
 import 'form_validation_demo.dart' as demo6 show title, makePage;
+import 'text_field_focus_demo.dart' as demo7 show title, makePage;
 
 final demoList = [
   {
@@ -25,5 +26,9 @@ final demoList = [
   {
     'name': demo6.title,
     'makePage': demo6.makePage
+  },
+  {
+    'name': demo7.title,
+    'makePage': demo7.makePage
   }
 ];
---
+++ lib/src/text_field_focus_demo.dart
@@ -0,0 +1,56 @@
+// https://flutter.io/docs/cookbook/forms/focus
+
+import 'package:flutter/material.dart';
+
+final title = 'Text Field Focus';
+
+Page makePage() => Page();
+
+class Page extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              autofocus: true,
+            ),
+            TextField(
+              focusNode: myFocusNode,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => FocusScope.of(context).requestFocus(myFocusNode),
+        tooltip: 'Focus Second Text Field',
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
